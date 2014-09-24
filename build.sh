6cc0de374c89c2ed78408971419ea33b0164e8fe
Vadim Velikodniy <vadim@velikodniy.name>
2014-09-24 23:10:47 +0300

    Изменено имя выходного файла

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-pandoc --latex-engine=xelatex -V tables -V lang:russian -V fontsize:10pt -H include nethack.md -o nh.pdf
+pandoc --latex-engine=xelatex -V tables -V lang:russian -V fontsize:10pt -H include nethack.md -o nethack.pdf
